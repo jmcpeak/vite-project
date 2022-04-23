5c4b50a0a4473228e577f7db8503e5649a1262d1
Jason McPeak <jmcpeak@payrailz.com>
2022-04-23 11:39:53 -0700

    get build to work

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,19 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default {
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis', // <-- AWS SDK
+      },
+    },
+  },
   resolve: {
     alias: {
       consts: resolve(__dirname, 'src/consts.js'),
       hooks: resolve(__dirname, 'src/hooks/index.js'),
-      './runtimeConfig': './runtimeConfig.browser',
+      './runtimeConfig': './runtimeConfig.browser', // <-- AWS SDK
     },
   },
   plugins: [react()],
